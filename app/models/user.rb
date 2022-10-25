9b90bc454a6ed0b761360d84c0b58898100c662d
vietanhp <vietanhpham1111@gmail.com>
2022-10-25 19:17:54 +0700

    basic_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,26 @@
 class User < ApplicationRecord
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 },
-		format: { with: VALID_EMAIL_REGEX },
-		uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
-	before_save { self.email = email.downcase }
+  before_save :downcase_email
 
-	has_secure_password
+  has_secure_password
 
-	private
-	def downcase_email
-		email.downcase!
-	end
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
+
+  private
+  def downcase_email
+    email.downcase!
+  end
 end
-                                                                                                                  
