301b0920d721a7001023eeb8fa25c7cf10650a2e
vietanhp <vietanhpham1111@gmail.com>
2022-11-02 20:24:45 +0700

    password_reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
@@ -9,5 +11,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
 
